11e6026e09a742d7283ac8ce87cd087e956c6a6e
vileton15 <vileton15@sps-prosek.cz>
2018-10-03 09:35:04 +0200

    eow

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,6 +23,53 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            VytvorPole();
+            
+        }
+        public void VytvorPole()
+        {
+            int ctrColumn = 0;
+            int ctrRow = 0;
+            
+            int MujCTR = 0;
+            
+            Grid DynamicGrid = test;
+            DynamicGrid.Background = new SolidColorBrush(Colors.LightSteelBlue);
+            DynamicGrid.ShowGridLines = true;
+            while(ctrColumn<5)
+            {
+                ColumnDefinition Column = new ColumnDefinition();
+                DynamicGrid.ColumnDefinitions.Add(Column);
+                RowDefinition gridRow1 = new RowDefinition();
+                DynamicGrid.RowDefinitions.Add(gridRow1);
+                ctrColumn++;
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                
+                for (int k = 0; k < 5; k++)
+                {
+                    Label Hodnota = new Label();
+                    Hodnota.VerticalAlignment = VerticalAlignment.Center;
+                    Hodnota.HorizontalAlignment = HorizontalAlignment.Center;
+                    Hodnota.Content = MujCTR;
+
+                    Button Btn = new Button();
+                    Btn.Click += myButton_Click;
+                    Grid.SetRow(Btn, j);
+                    Grid.SetColumn(Btn, k);
+                    DynamicGrid.Children.Add(Btn);
+                    Console.WriteLine(j + ":" + k);
+                    MujCTR++;
+                }
+            }
+        }
+        void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button _btn = sender as Button;
+            int _row = (int)_btn.GetValue(Grid.RowProperty);
+            int _column = (int)_btn.GetValue(Grid.ColumnProperty);
+            MessageBox.Show("řádek: "+_row+" sloupec: " + _column);
         }
     }
 }
