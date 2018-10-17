f0dc04f7f600ae4fa62d59fc225776b1d7f15ea0
vileton15 <vileton15@sps-prosek.cz>
2018-10-17 09:32:55 +0200

    kokotina

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -113,7 +113,6 @@ namespace WpfApp1
             Button _btn = sender as Button;
             int _row = (int)_btn.GetValue(Grid.RowProperty);
             int _column = (int)_btn.GetValue(Grid.ColumnProperty);
-            //MessageBox.Show("řádek: "+_row+" sloupec: " + _column);
             SpawnMine(_row, _column);
         }
         void RevealPole(int selRadek, int selSloupec)
@@ -124,39 +123,99 @@ namespace WpfApp1
             Grid.SetRow(Btn, selRadek);
             Grid.SetColumn(Btn, selSloupec);
             DynamicGrid.Children.Add(Btn);
-            /*
-            for (int o = 0; o < 3; o++)
-            {
 
-                for (int p = 0; p < 3; p++)
+
+
+            /*OdhalNuly(selRadek-1, selSloupec);
+            Console.WriteLine("Odhal bombu cyklus 1");
+            OdhalNuly(selRadek, selSloupec-1);
+            Console.WriteLine("Odhal bombu cyklus 2");
+            OdhalNuly(selRadek, selSloupec+1);
+            Console.WriteLine("Odhal bombu cyklus 3");
+            OdhalNuly(selRadek + 1, selSloupec);*/
+
+
+
+
+        }
+        public void SousedVolno(int selRadek,int selSloupec)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k < 3; k++)
                 {
-                    string VolnoOkoli1 = ((selRadek - 1) + o) + "" + ((selSloupec - 1) + p);
-                    var VolnoOkoliList1 = BombList.FirstOrDefault(stringToCheck => stringToCheck.Contains(VolnoOkoli1));
-                    if (VolnoOkoliList1 == null)
+                    RevealPole(((selRadek - 1) + j), ((selSloupec - 1) + k));
+                }
+            }
+        }
+        void OdhalNuly(int selRadek, int selSloupec)
+        {
+            while(true)
+            {
+                bool Found = false;
+                int Nalezeno = 0;
+                for (int o = 0; o < 3; o++)
+                {
+
+                    for (int p = 0; p < 3; p++)
                     {
-                        RevealPole2(((selRadek - 1) + o), ((selSloupec - 1) + p));
+                        if( (((selRadek - 1) + o) <0) || (((selRadek - 1) + o)>PocetPoli))
+                        {
+                            break;
+                        }
+                        else if((((selSloupec - 1) + o) < 0) || (((selSloupec - 1) + o) > PocetPoli))
+                        {
+                            break;
+                        }
+                        string VolnoOkoli1 = ((selRadek - 1) + o) + "" + ((selSloupec - 1) + p);
+                        var VolnoOkoliList1 = BombList.FirstOrDefault(stringToCheck => stringToCheck.Contains(VolnoOkoli1));
+                        if (VolnoOkoliList1 == null)
+                        {
+                            //RevealPole(((selRadek - 1) + o), ((selSloupec - 1) + p));
+                        }
+                        else
+                        {
+                            Nalezeno++;
+                            
+                            Found = true;
+                        }
                     }
                 }
+                
+                if (Found)
+                {
+                    Grid DynamicGrid = test;
+                    Button Btn = new Button();
+                    Btn.Content = "" + Nalezeno;
+                    if (Nalezeno == 1)
+                    {
+                        Btn.Foreground = new SolidColorBrush(Colors.Blue);
+                    }
+                    else if (Nalezeno == 2)
+                    {
+                        Btn.Foreground = new SolidColorBrush(Colors.Green);
+                    }
+                    Btn.Background = new SolidColorBrush(Colors.LightGray);
+                    Grid.SetRow(Btn, selRadek);
+                    Grid.SetColumn(Btn, selSloupec);
+                    DynamicGrid.Children.Add(Btn);
+                    break;
+                }
+                else
+                {
+                    //RevealPole(selRadek, selSloupec);
+                    break;
+                }
+            }
+            
+
+            /*
+            string NulaOkolo = (selRadek) + "" + (selSloupec);
+            var NulyOkolo = BombList.FirstOrDefault(stringToCheck => stringToCheck.Contains(NulaOkolo));
+            if(NulyOkolo != null)
+            {
+                
             }*/
-            
-            
-        }
-        void RevealPole2(int selRadek, int selSloupec)
-        {
-            if(selRadek<0)
-            {
-                selRadek = 0;
-            }
-            if(selSloupec<0)
-            {
-                selSloupec = 0;
-            }
-            Grid DynamicGrid = test;
-            Button Btn = new Button();
-            Btn.Background = new SolidColorBrush(Colors.LightGray);
-            Grid.SetRow(Btn, selRadek);
-            Grid.SetColumn(Btn, selSloupec);
-            DynamicGrid.Children.Add(Btn);
         }
         void SpawnMine(int selRadek,int selSloupec)
         {
